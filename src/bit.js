ff77bf4a13ae4ba3cefd77a5e956bd6d875a361f
Hiroshi Kori <hiroshi.maybe@gmail.com>
2014-06-06 00:48:16 -0700

    Implemented decimal to binary

--- src/bit.js
+++ src/bit.js
@@ -17,3 +17,28 @@ function compound(n, m, i, j) {
 
 console.log(compound('10000000000', '10101', 2, 6));
 
+// http://www.amazon.com/Cracking-Coding-Interview-Programming-Questions/dp/098478280X
+// Q 5.2
+
+function to_binary(n) {
+  var integer, dec, result = "", dec_result = "";
+  integer = Math.floor(n);
+  dec     = n - integer;
+  while (integer > 0) {
+    result = (integer % 2) + result;
+    integer >>= 1;
+  }
+  while (dec > 0) {
+    if (dec_result.length>32) return false;
+    dec *= 2;
+    if (dec>=1) {
+      dec_result += 1;
+      dec -= 1;
+    } else {
+      dec_result += 0;
+    }
+  }
+  return result+"."+dec_result;
+};
+
+console.log(to_binary(100.35));
