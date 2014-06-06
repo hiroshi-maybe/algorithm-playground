c2127d2ed65744a553b547da362432df78b86309
Hiroshi Kori <hiroshi.maybe@gmail.com>
2014-06-06 09:40:46 -0700

    Implemented binary search to find rotated index

--- src/binary_search.js
+++ src/binary_search.js
@@ -39,3 +39,24 @@ console.log(bsearch(a,9));
 console.log(bsearch(a,-1));
 console.log(bsearch(a,10));
 console.log(bsearch(a,3.5));
+
+var find_rotated_index = function(ar, start, end) {  
+  var i=0, len=ar.length, mid;
+
+  start = start==null ? 0 : start;
+  end = end==null ? ar.length-1 : end;
+
+  mid=Math.floor((start+end)/2);
+  if (ar[mid] <= ar[0])  {
+    if (ar[mid-1]==null) return 0;
+    if (ar[mid-1]>ar[mid]) return mid;
+    return find_rotated_index(ar, start, mid-1);
+  } else {
+    if (ar[mid+1]==null) return ar.length-1;
+    if (ar[mid+1]<ar[mid]) return (mid+1);
+    return find_rotated_index(ar, mid+1, end);
+  }
+};
+
+console.log("find rotated index");
+console.log(find_rotated_index([15,16,19,20,25,30,31,1,10,14]));
