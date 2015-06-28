58dbe98e2f4a12320f1a3dd38ba8ff8c786673de
Hiroshi Kori <hiroshi.maybe@gmail.com>
2015-06-28 11:59:38 -0700

    peano number written in Swift

---
+++ src/natural-number/peanoNumber.playground/Contents.swift
@@ -0,0 +1,48 @@
+//: Playground of peano number
+// http://www.fewbutripe.com/swift/math/2015/01/20/natural-numbers.html
+
+import UIKit
+
+var str = "Hello, playground"
+
+/*
+ * recursive type will be supported in Swift 2.0 in the future
+ *
+ * enum Nat {
+ *   case Zero
+ *   indirect case Succ(Nat)
+ * }
+ */
+
+enum Nat {
+    case Zero
+    case Succ(()->Nat)
+    
+    func toInt()->Int {
+        switch self {
+            case .Zero:
+                return 0
+            case .Succ(let nat):
+                return nat().toInt()+1
+        }
+    }
+}
+
+let zero: Nat = .Zero
+let one: Nat = .Succ({zero})
+let two: Nat = .Succ({one})
+let three: Nat = .Succ({two})
+let four: Nat = .Succ({.Succ({.Succ({.Succ({.Zero})})})})
+
+let fourInt = four.toInt()
+
+func add(a:Nat, b:Nat) -> Nat {
+    switch b {
+    case .Zero:
+        return a
+    case .Succ(let nat):
+        return add(.Succ({a}), nat())
+    }
+}
+
+add(one, three).toInt()
