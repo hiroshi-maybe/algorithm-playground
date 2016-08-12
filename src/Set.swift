0af2da617ec4747ab5826e6e8c61e880032ecc4c
Hiroshi Kori <hiroshi.maybe@gmail.com>
2016-08-12 00:04:17 -0700

    solve subset generator

--- src/Set.swift
+++ src/Set.swift
@@ -32,3 +32,27 @@ func intersection(ar1: [Int], _ ar2: [Int]) -> Set<Int> {
 }
 
 assert(intersection(ar1, ar2)==Set([15, 19, 21]))
+
+/////////////////////////////////
+
+// http://www.slideshare.net/gayle2/cracking-the-facebook-coding-interview
+//
+// All subsets
+// s = {"a", "b", "c"} -> [{}, {a}, {b}, {c}, {a, b}, ..., {a, b, c}]
+
+// time complexity: n + (n-1) + .. 1 = O(S^2), S = Size of set
+// space complexity: nCn + nCn-1 + ... + nC1 = 1 + n + .. n := O(S^2)?
+func generateSubset(orgSet: Set<String>) -> [Set<String>] {
+  guard orgSet.count > 0 else { return [Set()] }
+  var set = orgSet
+  let first = set.removeFirst()
+  
+  let subset = generateSubset(set) // call stack: S
+  
+  return subset + subset.map { (var setEl: Set<String>) in
+    setEl.insert(first)
+    return setEl
+  }
+}
+
+assert(generateSubset(["a", "b", "c"]).count == 8)
