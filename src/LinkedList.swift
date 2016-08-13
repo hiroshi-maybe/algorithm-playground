72a830c7e2c1752422fe1f4bb8a777681f4f9e85
Hiroshi Kori <hiroshi.maybe@gmail.com>
2016-08-13 12:08:17 -0700

    remove duplicated in linked list

--- src/LinkedList.swift
+++ src/LinkedList.swift
@@ -21,8 +21,15 @@ class Node: CustomDebugStringConvertible {
     return "\(v)->\(nextStr)"
   }
   
-  static func create(ints: [Int]) -> Node? {
-    let sorted = ints.sort().map { Node(v: $0) }
+  func deleteNext() {
+    guard let nextCurrent = next else { return }
+    
+    let nextNew = nextCurrent.next
+    self.next = nextNew
+  }
+  
+  static func create(vals: [Int]) -> Node? {
+    let sorted = vals.map { Node(v: $0) }
     
     Zip2Sequence(
       sorted[0..<sorted.count-1],
@@ -70,14 +77,55 @@ class Node: CustomDebugStringConvertible {
   }
 }
 
-if let node1 = Node.create([1, 3, 5, 7]),
-  node2 = Node.create([1, 2, 4, 10]) {
+if let node1 = Node.create([1, 3, 5, 7].sort()),
+  node2 = Node.create([1, 2, 4, 10].sort()) {
   let headMerged = Node.merge(node1, node2)
   assert("\(headMerged!)" == "1->1->2->3->4->5->7->10->nil")
 }
 
-if let node1 = Node.create([1]),
-  node2 = Node.create([1, 2, 4, 10]) {
+if let node1 = Node.create([1].sort()),
+  node2 = Node.create([1, 2, 4, 10].sort()) {
   let headMerged = Node.merge(node1, node2)
   assert("\(headMerged!)" == "1->1->2->4->10->nil")
 }
+
+
+/////////////////////////////////
+
+// https://www.amazon.com/Cracking-Coding-Interview-Programming-Questions/dp/098478280X
+// Q 2.1
+
+// 18 -> 7 -> 2 -> 89 -> 1 -> 2 -> 7 -> 18
+//                            *
+// found = { 18, 7, 2, 89, 1 }
+//
+// 18 -> 7 -> 2 -> 89 -> p:1 -> n:7 -> 18
+// found = { 18, 7, 2, 89, 1 }
+
+// 18 -> 7 -> 2 -> 89 -> p:1 -> n:18
+// 18 -> 7 -> 2 -> 89 -> p:1
+
+let headToDelDuplicate = Node.create([18, 7, 2, 89, 1, 2, 7, 18])
+
+func deleteDuplicatedInLinkedList(node: Node, prev: Node?, var found: Set<Int> = Set()) -> Node {
+  // `found` has values in head -> .. -> prev
+
+  let current: Node?
+  
+  if found.contains(node.v) {
+    prev?.next = node.next
+    current = prev
+  } else {
+    current = node
+  }
+  
+  found.insert(node.v)
+  
+  if let next = node.next {
+    deleteDuplicatedInLinkedList(next, prev: current, found: found)
+  }
+  
+  return node
+}
+
+assert("\(deleteDuplicatedInLinkedList(headToDelDuplicate!, prev: nil))" == "18->7->2->89->1->nil")
